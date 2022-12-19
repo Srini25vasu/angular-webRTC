0989b9249fc8d7a728827364b97328c9b84b22ab
Srini <srini25vasu@gmail.com>
2022-12-19 11:10:47 +0100

    added Socket IO module

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # angular-webRTC
+References: https://padymies.medium.com/video-chats-with-angular-and-webrtc-3f892dd0acd6
+npm install ngx-socket-io
--- angular-webrtc/src/app/app.module.ts
+++ angular-webrtc/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+const config: SocketIoConfig = { url: "http://localhost:4500", options: {}};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
